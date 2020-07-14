1b60d95da2d1cb2953bc7cf0b2d28b837e143419
moshi <moshiur@cefalo>
2020-07-15 01:46:29 +0600

    product added with seed data

--- src/Core/Models/ShopDbContext.cs
+++ src/Core/Models/ShopDbContext.cs
@@ -2,22 +2,24 @@
 
 namespace Core.Models
 {
-    public class ShopDbContext: DbContext
+    public class ShopDbContext : DbContext
     {
         public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options)
         {
-           
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new UserConfiguration());
             modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
             base.OnModelCreating(modelBuilder);
         }
 
         public DbSet<User> Users { get; set; }
 
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
